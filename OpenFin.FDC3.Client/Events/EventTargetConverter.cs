fd6ebfb33d7b7ad3ffea6f1e564b3c5eafd526c9
Philip Broadbent <philip@openfin.co>
2019-09-27 19:23:56 +0100

    [SERVICE-737] Fix for deserialisation issue caused by the union type used by 'Targeted' transport type.

---
+++ OpenFin.FDC3.Client/Events/EventTargetConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace OpenFin.FDC3.Events
+{
+    public class EventTargetConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String && serializer.Deserialize<string>(reader) == "default")
+            {
+                // TBD: How to handle events targeted at the 'default' emitter.
+                // Currently using a "null, null" target to represent the default emitter.
+                return new EventTransportTarget();
+            }
+            else
+            {
+                return serializer.Deserialize(reader, objectType);
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // We don't need to send EventTarget objects to the provider, we will only ever receive them
+            throw new NotImplementedException();
+        }
+    }
+}
